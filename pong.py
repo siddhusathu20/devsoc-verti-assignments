3b9238099c8f8c39fc30dfd9396a9dc57a3847a6
siddhusathu20 <53175949+siddhusathu20@users.noreply.github.com>
2025-10-26 23:00:52 +0530

    Add files via upload

--- pong.py
+++ pong.py
@@ -6,12 +6,29 @@ ball_speed_x = 7 * random.choice((1, -1))
 ball_speed_y = 7 * random.choice((1, -1))
 player_speed = 0
 opponent_speed = 0
+game_in_prog = True
 
 def game_over():
-    global ball_speed_x, ball_speed_y
-    ball.center = (screen_width / 2, screen_height / 2)
-    ball_speed_x *= random.choice((1, -1))
-    ball_speed_y *= random.choice((1, -1))
+    global game_in_prog, player_speed, opponent_speed
+    game_in_prog = False
+    player_speed = 0
+    opponent_speed = 0
+
+    screen.fill((0, 0, 0))
+
+    font_title = pygame.font.SysFont("Bahnschrift", 75)
+    font_subtitle = pygame.font.SysFont("Bahnschrift", 35)
+
+    game_over_text = font_title.render("GAME OVER", True, (255, 0, 0))
+    game_over_rect = game_over_text.get_rect(center=(screen.get_width() // 2, screen.get_width() // 3))
+    screen.blit(game_over_text, game_over_rect)
+
+    restart_text = font_subtitle.render("Press R to restart", True, (255, 0, 0))
+    restart_rect = restart_text.get_rect(center=(screen.get_width() // 2, screen.get_width() * 2 // 3))
+    screen.blit(restart_text, restart_rect)
+
+    pygame.display.flip()
+    clock.tick(60)
 
 def move_ball():
     global ball_speed_x, ball_speed_y
@@ -54,38 +71,51 @@ player_paddle = pygame.Rect(screen_width - 20, screen_height/2 - 70, 10, 140)
 opponent_paddle = pygame.Rect(10, screen_height/2 - 70, 10, 140)
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_DOWN:
-                player_speed += 7
-            if event.key == pygame.K_UP:
-                player_speed -= 7
-            if event.key == pygame.K_s:
-                opponent_speed += 7
-            if event.key == pygame.K_w:
-                opponent_speed -= 7
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_DOWN:
-                player_speed -= 7
-            if event.key == pygame.K_UP:
-                player_speed += 7
-            if event.key == pygame.K_s:
-                opponent_speed -= 7
-            if event.key == pygame.K_w:
-                opponent_speed += 7
-    
-    move_ball()
-    move_player()
-    move_opponent()
-    
-    screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, (0, 255, 255), player_paddle)
-    pygame.draw.rect(screen, (0, 255, 255), opponent_paddle)
-    pygame.draw.ellipse(screen, (0, 255, 255), ball)
-    pygame.draw.aaline(screen, (0, 255, 255), (screen_width / 2, 0), (screen_width / 2, screen_height))
+    if game_in_prog:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    player_speed += 7
+                if event.key == pygame.K_UP:
+                    player_speed -= 7
+                if event.key == pygame.K_s:
+                    opponent_speed += 7
+                if event.key == pygame.K_w:
+                    opponent_speed -= 7
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_DOWN:
+                    player_speed -= 7
+                if event.key == pygame.K_UP:
+                    player_speed += 7
+                if event.key == pygame.K_s:
+                    opponent_speed -= 7
+                if event.key == pygame.K_w:
+                    opponent_speed += 7
+        
+        move_ball()
+        move_player()
+        move_opponent()
+        
+        screen.fill((0, 0, 0))
+        pygame.draw.rect(screen, (0, 255, 255), player_paddle)
+        pygame.draw.rect(screen, (0, 255, 255), opponent_paddle)
+        pygame.draw.ellipse(screen, (0, 255, 255), ball)
+        pygame.draw.aaline(screen, (0, 255, 255), (screen_width / 2, 0), (screen_width / 2, screen_height))
 
-    pygame.display.flip()
-    clock.tick(60)
+        pygame.display.flip()
+        clock.tick(60)
+    else:
+        game_over()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    ball.center = (screen_width / 2, screen_height / 2)
+                    ball_speed_x *= random.choice((1, -1))
+                    ball_speed_y *= random.choice((1, -1))
+                    game_in_prog = True
